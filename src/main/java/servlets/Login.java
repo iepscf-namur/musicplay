fe4e4b53fa689c06fb34b293565adf07dd6632f6
Pecheur Robin <truc@mail.com>
2018-10-19 10:25:20 +0200

    première étape de la page login correction d'un paramètre empêchant le not de passe d'être lu Premier test simple qui aiguille vers accueil sur le login et le password ont une valeur      sinon retour à la page login
    Pour les prochaines modifications:
         trouver comment traiter les erreurs dans le doPost() sans devoir refaire appel au doGet()

--- src/main/java/servlets/Login.java
+++ src/main/java/servlets/Login.java
@@ -11,20 +11,22 @@ import java.io.IOException;
 public class Login extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String login = request.getParameter("login");
-        String pass = request.getParameter("Pass");
+        String pass = request.getParameter("pass");
 
-        System.out.println(login);
+        System.out.println(login+" "+pass);
 
         // Call WebService
         // If WebService successfull
         // Dans l'écran accueil : message "user login connecté
-
+        if((login!=null&&login!="")&&(pass!=null&&pass!=""))
         this.getServletContext().getRequestDispatcher("/WEB-INF/accueil.jsp").forward(request, response);
+        //remarque login et pass ne seront null que s'il ne sont pas instancié (donc pas lier aux bons éléments)
 
         // If WebService not successfull
         // Retry dans l'écran Login
-
-        // A ENLEVER this.getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+        else
+        this.getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+        //A laisser pour le moment : sinon plus d'affichage de la page après le premier essai
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
